833ce9c3f529507cbad3e7dbf07d421905b4a33c
Ufuka <61376126+Ufuk-a@users.noreply.github.com>
2020-12-15 22:08:46 +0300

    ..

--- litevision/lib/vision/frc_2018_vision/vision_2018.py
+++ litevision/lib/vision/frc_2018_vision/vision_2018.py
@@ -1,10 +1,14 @@
 import cv2 as cv
 import numpy as np
 
-def process_18():    
+def process_18(img_bgr):    
     
+
     min_green = np.array([57, 100, 100])
     max_green = np.array([97, 255, 255])
+    
+    img = cv.cvtColor(img_bgr, cv.COLOR_BGR2HSV)
+
     mask = cv.inRange(img, min_green, max_green)
     contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     for contour in contours:
@@ -15,8 +19,7 @@ def process_18():
         box = np.int0(box)
         cv.drawContours(img_bgr,[box],0,(0,255,0),2)
             
-    cv.imshow("img", img_bgr)
-    cv.imshow("mask", mask)
+    
         
     
     
