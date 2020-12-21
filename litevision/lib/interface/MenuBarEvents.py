b11665fd35a575b2bf8a60139c634af3edf794ac
Yüşa Furkan <71369719+yehpop@users.noreply.github.com>
2020-12-21 16:45:43 +0300

    streaming from camera and video processing 🥳

--- litevision/lib/interface/MenuBarEvents.py
+++ litevision/lib/interface/MenuBarEvents.py
@@ -4,7 +4,6 @@ import pygame_gui
 
 from litevision.res.glob import *
 from litevision.lib.database import *
-from litevision.lib import vision
 
 
 class HandlerForMenuBarEvents:
@@ -13,13 +12,14 @@ class HandlerForMenuBarEvents:
         self.ui_manager = ui_manager
 
         self.stream_started = False
+        self.processing = False
 
         # bunu kullanır mıyız emin değilim direkt eventle de halledebilirz sanırım ama yeniden açıldığında
         # son bırakıldığı halde olması için diye düşündüm
         self.litevison_settings = read_json(LITEVISION_SETTINGS_PATH)
         self.theme_file = read_json(THEME_FILE)
 
-    def process_events(self, event, menu_bar):
+    def process_events(self, event, menu_bar, stream):
         if (event.type == pygame.USEREVENT
                 and event.user_type == pygame_gui.UI_BUTTON_START_PRESS
                 and event.ui_object_id == '#menu_bar.#start_pause'):
@@ -37,7 +37,11 @@ class HandlerForMenuBarEvents:
                 self.stream_started = False
                 event.ui_element.set_image(sp_icon)
                 pygame.event.post(GUI_STREAM_WINDOW_PAUSE)
+
         if (event.type == pygame.USEREVENT
                 and event.user_type == pygame_gui.UI_BUTTON_START_PRESS
                 and event.ui_object_id == '#menu_bar.#start_processing'):
-            pass
+            if self.processing == False and self.stream_started:
+                self.processing = True
+            else:
+                self.processing = False
